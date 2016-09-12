97f03d0ce7f03e4d24d4c01759aaefdb49db1116
seankross <smk240@gmail.com>
2016-09-12 16:48:43 -0400

    added rpe

--- build.R
+++ build.R
@@ -1,20 +1,26 @@
 library(yaml)
 library(swirlify)
 
-ctime <- as.integer(Sys.time())
-clone_dir <- file.path(tempdir(), ctime)
-dir.create(clone_dir, showWarnings = FALSE)
-git_clone <- paste("git clone https://github.com/swirldev/swirl_courses.git", clone_dir)
-system(git_clone)
+course_list <- yaml.load_file("courses.yaml")
 
-courses <- file.path(clone_dir, swirl:::make_pathname(yaml.load_file("courses.yaml")))
-
-for(i in courses){
-  # Find any lesson
-  lesson <- list.files(i, recursive = TRUE, 
-                       pattern = "lesson", full.names = TRUE)[1]
-  set_lesson(path_to_yaml = lesson, open_lesson = FALSE, silent = TRUE)
-  pack_course(export_path = ".")
+for(i in seq_along(course_list)){
+  key <- course_list[[i]]
+  dir.create(file.path(".", names(course_list)[i]))
+  git_clone <- paste("git clone", key$git_clone_url, file.path(".", names(course_list)[i]))
+  system(git_clone)
+  if(!is.null(key$subdirs)){
+    courses <- file.path(".", names(course_list)[i], key$subdirs)
+    for(j in courses){
+      lesson <- list.files(j, recursive = TRUE, 
+                           pattern = "lesson", full.names = TRUE)[1]
+      set_lesson(path_to_yaml = lesson, open_lesson = FALSE, silent = TRUE)
+      pack_course(export_path = ".")
+    }
+  } else {
+    lesson <- list.files(file.path(".", names(course_list)[i]), recursive = TRUE, 
+                         pattern = "lesson", full.names = TRUE)[1]
+    set_lesson(path_to_yaml = lesson, open_lesson = FALSE, silent = TRUE)
+    swc_path <- pack_course(export_path = ".")
+  }
+  unlink(file.path(".", names(course_list)[i]), recursive = TRUE, force = TRUE)
 }
-
-# for(i in list.files(pattern = "swc$")){unpack_course(i, ".")}
